8cef5b00dec11bcb1802ab72625a7be077234c75
kohlihui <k.l.hui2003@gmail.com>
2023-12-19 11:52:34 +0800

    Update sidebar_menu.php

--- StuPort/app/views/includes/sidebar_menu.php
+++ StuPort/app/views/includes/sidebar_menu.php
@@ -60,11 +60,6 @@
 
 									<!--begin:Menu item-->
 									<div class="menu-item pt-5">
-											<!--begin:Menu content-->
-											<div class="menu-content">
-												<span class="menu-heading fw-bold text-uppercase fs-7">Activity Feedback</span>
-											</div>
-											<!--end:Menu content-->
 										</div>
 										<!--end:Menu item-->
 										<!--begin:Menu item-->
