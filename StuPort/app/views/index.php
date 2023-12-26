9113a91534df2d661bdbe59445c3c2bff56c6e2e
Koh Su Xuan <ksx0428@gmail.com>
2023-12-26 11:37:26 +0800

    Update index.php

--- StuPort/app/views/index.php
+++ StuPort/app/views/index.php
@@ -11,9 +11,9 @@
                         <div id="kt_app_content" class="app-content flex-column-fluid">
                             <!--begin::Content container-->
                             <div id="kt_app_content_container" class="app-container container-fluid">
-                                <!--begin::Row-->
+                            <!--begin::Row-->
                             
-                          
+                            <?php echo "Hello"?>
                             
 
                         
