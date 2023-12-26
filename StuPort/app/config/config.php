87a5b759002ad8042df7feba70466cebcd155cc4
Koh Su Xuan <ksx0428@gmail.com>
2023-12-26 10:18:51 +0800

    Update config.php

--- StuPort/app/config/config.php
+++ StuPort/app/config/config.php
@@ -3,30 +3,24 @@
 define('DB_HOST', 'localhost'); //Add your db host
 define('DB_USER', 'root'); // Add your DB root
 define('DB_PASS', ''); //Add your DB pass
-<<<<<<< Updated upstream
-<<<<<<< HEAD
 define('DB_NAME', 'niagaped_explorer'); //Add your DB Name
-=======
-define('DB_NAME', 'mvcframework'); //Add your DB Name
->>>>>>> 91c9b908c57c339e21ab906ef8263e113568b81e
-=======
-define('DB_NAME', 'niagaped_Explorer'); //Add your DB Name
->>>>>>> Stashed changes
 
 /*//Database params
 define('DB_HOST', 'localhost'); //Add your db host
 define('DB_USER', 'niagaped_Explorer_user'); // Add your DB root
 define('DB_PASS', 'Explorer&^^%&*'); //Add your DB pass
+define('DB_NAME', 'niagaped_Explorer'); //Add your DB Name
 define('DB_NAME', 'niagaped_Explorer'); //Add your DB Name */
 
 //APPROOT
 define('APPROOT', dirname(dirname(__FILE__)));
 
 //URLROOT (Dynamic links)
-define('URLROOT', 'http://localhost/niagaped_Explorer');
+define('URLROOT', 'http://localhost/explorer/StuPort');
 
 //URLROOT (Dynamic links)
-//define('URLROOT', 'https://sandkas.com/Explorer/');
+// define('URLROOT', 'https://sandkas.com/Explorer/');
 
 //Sitename
 define('SITENAME', 'Login & Register script');
+?>
