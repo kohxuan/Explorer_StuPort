f10067bdf114d9ebdbe26c99b3158a85d702b122
Koh Su Xuan <ksx0428@gmail.com>
2023-12-26 09:36:12 +0800

    Update config.php

--- StuPort/app/config/config.php
+++ StuPort/app/config/config.php
@@ -1,15 +1,24 @@
 <?php
-    //Database params
-    define('DB_HOST', 'localhost'); //Add your db host
-    define('DB_USER', 'root'); // Add your DB root
-    define('DB_PASS', ''); //Add your DB pass
-    define('DB_NAME', 'mvcframework'); //Add your DB Name
+//Database params
+// define('DB_HOST', 'localhost'); //Add your db host
+// define('DB_USER', 'root'); // Add your DB root
+// define('DB_PASS', ''); //Add your DB pass
+// define('DB_NAME', 'mvcframework'); //Add your DB Name
 
-    //APPROOT
-    define('APPROOT', dirname(dirname(__FILE__)));
+//Database params
+define('DB_HOST', 'localhost'); //Add your db host
+define('DB_USER', 'niagaped_Explorer_user'); // Add your DB root
+define('DB_PASS', 'Explorer&^^%&*'); //Add your DB pass
+define('DB_NAME', 'niagaped_Explorer'); //Add your DB Name
 
-    //URLROOT (Dynamic links)
-    define('URLROOT', 'http://localhost/mvcprojectnew');
+//APPROOT
+define('APPROOT', dirname(dirname(__FILE__)));
 
-    //Sitename
-    define('SITENAME', 'Login & Register script');
+//URLROOT (Dynamic links)
+// define('URLROOT', 'http://localhost/mvcprojectnew');
+
+//URLROOT (Dynamic links)
+define('URLROOT', 'https://sandkas.com/Explorer/');
+
+//Sitename
+define('SITENAME', 'Login & Register script');
