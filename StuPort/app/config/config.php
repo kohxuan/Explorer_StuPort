7f665c5233c62c8ba8e8c1b613553f7f5def7c09
Koh Su Xuan <ksx0428@gmail.com>
2023-12-26 10:12:44 +0800

    Update config.php

--- StuPort/app/config/config.php
+++ StuPort/app/config/config.php
@@ -3,7 +3,7 @@
 define('DB_HOST', 'localhost'); //Add your db host
 define('DB_USER', 'root'); // Add your DB root
 define('DB_PASS', ''); //Add your DB pass
-define('DB_NAME', 'niagaped_Explorer'); //Add your DB Name
+define('DB_NAME', 'niagaped_explorer'); //Add your DB Name
 
 //Database params
 /*define('DB_HOST', 'localhost'); //Add your db host
