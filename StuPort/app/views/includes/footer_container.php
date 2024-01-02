938e09f238a42a4410dac9e05f703c8a5836cc7c
Koh Su Xuan <ksx0428@gmail.com>
2024-01-02 12:51:30 +0800

    Design

--- StuPort/app/views/includes/footer_container.php
+++ StuPort/app/views/includes/footer_container.php
@@ -4,7 +4,7 @@
 								<!--begin::Copyright-->
 								<div class="text-gray-900 order-2 order-md-1">
 									<span class="text-muted fw-semibold me-1">2023&copy;</span>
-									<a href="https://keenthemes.com" target="_blank" class="text-gray-800 text-hover-primary">Keenthemes</a>
+									<a href="https://keenthemes.com" target="_blank" class="text-gray-800 text-hover-primary">Explorer</a>
 								</div>
 								<!--end::Copyright-->
 								<!--begin::Menu-->
@@ -15,9 +15,9 @@
 									<li class="menu-item">
 										<a href="https://devs.keenthemes.com" target="_blank" class="menu-link px-2">Support</a>
 									</li>
-									<li class="menu-item">
+									<!-- <li class="menu-item">
 										<a href="https://1.envato.market/EA4JP" target="_blank" class="menu-link px-2">Purchase</a>
-									</li>
+									</li> -->
 								</ul>
 								<!--end::Menu-->
 							</div>
