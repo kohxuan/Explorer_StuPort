2568e96ead66388572a1480de9ecb0e3398c3106
kohlihui <k.l.hui2003@gmail.com>
2024-01-06 16:45:23 +0800

    update dashboard_student.php

--- StuPort/app/views/pages/index_student.php
+++
@@ -1,7 +0,0 @@
-<?php require APPROOT . '/views/includes/head_metronic.php';?>
-<?php require APPROOT . '/views/includes/begin_app_student.php';?>
-
-<?php require APPROOT . '/views/pages/layout.php'; ?>  
-                
-<?php require APPROOT . '/views/includes/end_app.php'; ?>
-<?php require APPROOT . '/views/includes/footer_metronic.php';?>
