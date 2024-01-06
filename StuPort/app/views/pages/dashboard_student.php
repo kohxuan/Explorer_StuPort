3d22c7dbbe7c00e9b84528a80daebafef48a988b
kohlihui <k.l.hui2003@gmail.com>
2024-01-06 23:18:03 +0800

    update

--- StuPort/app/views/pages/dashboard_student.php
+++ StuPort/app/views/pages/dashboard_student.php
@@ -32,21 +32,18 @@
     data-kt-app-sidebar-push-footer="true" data-kt-app-toolbar-enabled="true" class="app-default">
     <!--begin::Theme mode setup on page load-->
     <script>var defaultThemeMode = "light"; var themeMode; if (document.documentElement) { if (document.documentElement.hasAttribute("data-bs-theme-mode")) { themeMode = document.documentElement.getAttribute("data-bs-theme-mode"); } else { if (localStorage.getItem("data-bs-theme") !== null) { themeMode = localStorage.getItem("data-bs-theme"); } else { themeMode = defaultThemeMode; } } if (themeMode === "system") { themeMode = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light"; } document.documentElement.setAttribute("data-bs-theme", themeMode); }</script>
-    <!--end::Theme mode setup on page load--> </body>
-
-
-<!--begin::App-->
+    
     <div class="d-flex flex-column flex-root app-root" id="kt_app_root">
-        <!--begin::Page-->
+        
         <div class="app-page flex-column flex-column-fluid" id="kt_app_page">
-            <!--begin::Header-->
+            
             <div id="kt_app_header" class="app-header" data-kt-sticky="true"
                 data-kt-sticky-activate="{default: true, lg: true}" data-kt-sticky-name="app-header-minimize"
                 data-kt-sticky-offset="{default: '200px', lg: '0'}" data-kt-sticky-animation="false">
-                <!--begin::Header container-->
+                
                 <div class="app-container container-fluid d-flex align-items-stretch justify-content-between"
                     id="kt_app_header_container">
-                    <!--begin::Sidebar mobile toggle-->
+                    
                     <div class="d-flex align-items-center d-lg-none ms-n3 me-1 me-md-2" title="Show sidebar menu">
                         <div class="btn btn-icon btn-active-color-primary w-35px h-35px"
                             id="kt_app_sidebar_mobile_toggle">
@@ -56,19 +53,14 @@
                             </i>
                         </div>
                     </div>
-                    <!--end::Sidebar mobile toggle-->
-                    <!--begin::Mobile logo-->
                     <div class="d-flex align-items-center flex-grow-1 flex-lg-grow-0">
                         <a href="index.html" class="d-lg-none">
                             <img alt="Logo" src="<?php echo URLROOT ?>/public/assets/media/logos/default-small.svg"
                                 class="h-30px" />
                         </a>
                     </div>
-                    <!--end::Mobile logo-->
-                    <!--begin::Header wrapper-->
                     <div class="d-flex align-items-stretch justify-content-between flex-lg-grow-1"
                         id="kt_app_header_wrapper">
-                        <!--begin::Menu wrapper-->
 
                         <div class="app-header-menu app-header-mobile-drawer align-items-stretch" data-kt-drawer="true"
                             data-kt-drawer-name="app-header-menu" data-kt-drawer-activate="{default: true, lg: false}"
@@ -76,14 +68,14 @@
                             data-kt-drawer-toggle="#kt_app_header_menu_toggle" data-kt-swapper="true"
                             data-kt-swapper-mode="{default: 'append', lg: 'prepend'}"
                             data-kt-swapper-parent="{default: '#kt_app_body', lg: '#kt_app_header_wrapper'}">
-                            <!--begin::Menu-->
+                            
                             <div class="menu menu-rounded menu-column menu-lg-row my-5 my-lg-0 align-items-stretch fw-semibold px-2 px-lg-0"
                                 id="kt_app_header_menu" data-kt-menu="true">
-                                <!--begin:Menu item-->
+                                
                                 <div data-kt-menu-trigger="{default: 'click', lg: 'hover'}"
                                     data-kt-menu-placement="bottom-start"
                                     class="menu-item here show menu-here-bg menu-lg-down-accordion me-0 me-lg-2">
-                                    <!--begin:Menu link-->
+                                    
                                     <span class="menu-link">
                                         <span class="menu-title">Welcome
                                             <?php 
@@ -96,40 +88,13 @@
                                         </span>
                                         <span class="menu-arrow d-lg-none"></span>
                                     </span>
-                                    <!--end:Menu link-->
+                                    
                                 </div>
-                                <!--end:Menu item-->
+                                
                             </div>
-                            <!--end::Menu-->
+                            
                         </div>
 
-                       <html lang="en">
-<!--begin::Head-->
-
-<head>
-    <title>StuPort</title>
-    <meta charset="utf-8" />
-    <link rel="shortcut icon" href="<?php echo URLROOT ?>/public/assets/media/logos/yvtablogo.png" />
-    <!--begin::Fonts(mandatory for all pages)-->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700" />
-    <!--end::Fonts-->
-    <!--begin::Vendor Stylesheets(used for this page only)-->
-    <link href="<?php echo URLROOT ?>/public/assets/plugins/custom/fullcalendar/fullcalendar.bundle.css"
-        rel="stylesheet" type="text/css" />
-    <link href="<?php echo URLROOT?>/public/assets/plugins/custom/datatables/datatables.bundle.css" rel="stylesheet"
-        type="text/css" />
-
-    <!--end::Vendor Stylesheets-->
-    <!--begin::Global Stylesheets Bundle(mandatory for all pages)-->
-    <link href="<?php echo URLROOT ?>/public/assets/plugins/global/plugins.bundle.css" rel="stylesheet"
-        type="text/css" />
-    <link href="<?php echo URLROOT ?>/public/assets/css/style.bundle.css" rel="stylesheet" type="text/css" />
-
-    <!--end::Global Stylesheets Bundle-->
-    <script>// Frame-busting to prevent site from being loaded within a frame without permission (click-jacking) if (window.top != window.self) { window.top.location.replace(window.self.location.href); }</script>
-</head>
-<!--end::Head-->
-<!--begin::Body-->
 
 <body id="kt_app_body" data-kt-app-layout="dark-sidebar" data-kt-app-header-fixed="true"
     data-kt-app-sidebar-enabled="true" data-kt-app-sidebar-fixed="true" data-kt-app-sidebar-hoverable="true"
@@ -192,7 +157,7 @@
                                     <span class="menu-link">
                                         <span class="menu-title">Welcome
                                             <?php 
-											if ( isset( $_SESSION['username']) && isset( $_SESSION['user_role'])){
+											if ( isset( $_SESSION['username'])){
 											echo $_SESSION['user_role'] . "<br>" . $_SESSION['username'];
 											} else {
 												header('location: ' . URLROOT . '/users/login');
