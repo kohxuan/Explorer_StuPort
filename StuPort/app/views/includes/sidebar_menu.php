cc63d2071aa8dbdccd0f4a7563f7293499d8a10d
Koh Su Xuan <ksx0428@gmail.com>
2024-01-07 18:17:10 +0800

    Update sidebar_menu.php

--- StuPort/app/views/includes/sidebar_menu.php
+++ StuPort/app/views/includes/sidebar_menu.php
@@ -177,48 +177,6 @@
 				</div>
 				<!--end:Menu item-->
 
-
-				<!-- Resume -->
-				<!--begin:Menu item-->
-				<div class="menu-item pt-5">
-					<!--begin:Menu content-->
-					<div class="menu-content">
-						<span class="menu-heading fw-bold text-uppercase fs-7">Resume</span>
-					</div>
-					<!--end:Menu content-->
-				</div>
-				<!--end:Menu item-->
-				<!--begin:Menu item-->
-				<div data-kt-menu-trigger="click" class="menu-item menu-accordion">
-					<!--begin:Menu link-->
-					<span class="menu-link">
-						<span class="menu-icon">
-							<i class="ki-duotone ki-element-7 fs-2">
-								<span class="path1"></span>
-								<span class="path2"></span>
-							</i>
-						</span>
-						<span class="menu-title">Resume Option</span>
-						<span class="menu-arrow"></span>
-					</span>
-					<!--end:Menu link-->
-					<!--begin:Menu sub-->
-					<div class="menu-sub menu-sub-accordion">
-						<!--begin:Menu item-->
-						<div class="menu-item">
-							<!--begin:Menu link-->
-							<a class="menu-link" href="resume.php?user_id=<?php echo $userID; ?>" target="_blank">
-								<span class="menu-bullet">
-									<span class="bullet bullet-dot"></span>
-								</span>
-								<span class="menu-title">Download Resume</span>
-							</a>
-							<!--end:Menu link-->
-						</div>
-						<!--end:Menu item-->
-					</div>
-				</div>
-
 				<div class="menu-item pt-5">
 					<!--begin:Menu content-->
 					<div class="menu-content">
