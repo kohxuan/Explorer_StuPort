8d73af6d334befba765ef288038495e25e2bf381
YIKHONGLEE <128198038+YIKHONGLEE@users.noreply.github.com>
2024-01-11 12:53:38 +0800

    login interface

--- StuPort/app/views/users/login.php
+++ StuPort/app/views/users/login.php
@@ -102,9 +102,11 @@
                             <!--end::Wrapper-->
                             <!--begin::Submit button-->
                             <div class="d-grid mb-10">
-                                <button type="submit" id="kt_sign_in_submit" class="btn btn-primary">
+                            <button type="submit" id="kt_sign_in_submit" class="btn" style="background-color: #FCBD32; color: #ffffff;">Sign In</button>
+
+                                
                                     <!--begin::Indicator label-->
-                                    <span class="indicator-label">Sign In</span>
+                            
                                     <!--end::Indicator label-->
                                     <!--begin::Indicator progress-->
                                     <span class="indicator-progress">Please wait...
@@ -129,31 +131,30 @@
             <!--end::Body-->
             <!--begin::Aside-->
             <div class="d-flex flex-lg-row-fluid w-lg-50 bgi-size-cover bgi-position-center order-1 order-lg-2"
-                style="background-image: url(<?php echo URLROOT ?>/public/assets/media/misc/auth-bg.png)">
+                 style="background-image: url('https://i.ibb.co/vqRd3P2/Yv-logo-login.jpg');"> 
                 <!--begin::Content-->
-                <div class="d-flex flex-column flex-center py-7 py-lg-15 px-5 px-md-15 w-100">
+                <div class="d-flex flex-column flex-center py-7 py-lg-15 px-5 px-md-15 w-100 align-self-start">
                     <!--begin::Logo-->
                     <a href="index.html" class="mb-0 mb-lg-12">
-                        <img alt="Logo" src="<?php echo URLROOT ?>/public/assets/media/logos/custom-1.png" class="h-60px h-lg-75px" />
+                    <img src="https://i.ibb.co/7byWKRt/youth-venture-logo.png" alt="youth-venture-logo " style="height: 80px; margin-right: 10px;">
                     </a>
                     <!--end::Logo-->
                     <!--begin::Image-->
-                    <img class="d-none d-lg-block mx-auto w-275px w-md-50 w-xl-500px mb-10 mb-lg-20"
-                        src="<?php echo URLROOT ?>/public/assets/media/misc/auth-screens.png" alt="" />
-                    <!--end::Image-->
+                    
                     <!--begin::Title-->
-                    <h1 class="d-none d-lg-block text-white fs-2qx fw-bolder text-center mb-7">Fast, Efficient and
-                        Productive</h1>
+          
+                    <h1 class="d-none d-lg-block text-white fs-2qx fw-bolder text-center mb-7">Expert In Youth</h1>
                     <!--end::Title-->
                     <!--begin::Text-->
-                    <div class="d-none d-lg-block text-white fs-base text-center">In this kind of post,
+                    <!-- <div class="d-none d-lg-block text-white fs-base text-center">In this kind of post,
                         <a href="#" class="opacity-75-hover text-warning fw-bold me-1">the blogger</a>introduces a
                         person they’ve interviewed
                         <br />and provides some background information about
                         <a href="#" class="opacity-75-hover text-warning fw-bold me-1">the interviewee</a>and their
                         <br />work following this is a transcript of the interview.
-                    </div>
+                    </div> -->
                     <!--end::Text-->
+                    
                 </div>
                 <!--end::Content-->
             </div>
