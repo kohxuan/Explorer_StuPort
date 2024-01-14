2e1f798d503566737465026f2ccbef7e24fc5021
Koh Su Xuan <ksx0428@gmail.com>
2024-01-15 03:18:11 +0800

    Update navbar.php

--- StuPort/app/views/includes/navbar.php
+++ StuPort/app/views/includes/navbar.php
@@ -1,57 +1,20 @@
 <div class="app-navbar flex-shrink-0">
 	<!--begin::Search-->
 
-	
+
 
 	<!--begin::Activities-->
 	<div class="app-navbar-item ms-1 ms-md-4">
-			<!-- <input class="form-control me-2" type="search" placeholder="Search...." aria-label="Search" style="font-size: 14px; padding: 7px 10px; margin-right: 20px;"> btn btn-sm btn-primary align-self-center -->
-			<!-- <button class="btn btn-outline-success" type="submit" style="font-size: 14px; padding: 7px 10px; margin-right: 20px;">Search</button> -->
-			<!--begin::Button-->
-			<a href="<?php echo URLROOT; ?>/pages/index" class="btn btn-light-primary" style="font-size: 14px; padding: 7px 10px; margin-right: 20px;" >Homepage</a>
-			<!--end::Button-->
-		<!--begin::Drawer toggle-->
-		<div class="btn btn-icon btn-custom btn-icon-muted btn-active-light btn-active-color-primary w-35px h-35px" id="kt_activities_toggle">
-			<i class="ki-duotone ki-messages fs-2">
-				<span class="path1"></span>
-				<span class="path2"></span>
-				<span class="path3"></span>
-				<span class="path4"></span>
-				<span class="path5"></span>
-			</i>
-		</div>
-		<!--end::Drawer toggle-->
+		<!-- <input class="form-control me-2" type="search" placeholder="Search...." aria-label="Search" style="font-size: 14px; padding: 7px 10px; margin-right: 20px;"> btn btn-sm btn-primary align-self-center -->
+		<!-- <button class="btn btn-outline-success" type="submit" style="font-size: 14px; padding: 7px 10px; margin-right: 20px;">Search</button> -->
+		<!--begin::Button-->
+		<a href="<?php echo URLROOT; ?>/pages/index" class="btn btn-light-primary" style="font-size: 14px; padding: 7px 10px; margin-right: 20px;">Homepage</a>
+		<!--end::Button-->
+
 	</div>
 	<!--end::Activities-->
-	<!--begin::Notifications-->
-	<div class="app-navbar-item ms-1 ms-md-4">
-		<!--begin::Menu- wrapper-->
-		<div class="btn btn-icon btn-custom btn-icon-muted btn-active-light btn-active-color-primary w-35px h-35px" data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-attach="parent" data-kt-menu-placement="bottom-end" id="kt_menu_item_wow">
-			<i class="ki-duotone ki-notification-status fs-2">
-				<span class="path1"></span>
-				<span class="path2"></span>
-				<span class="path3"></span>
-				<span class="path4"></span>
-			</i>
-		</div>
 
-		<!--end::Menu wrapper-->
-	</div>
-	<!--end::Notifications-->
-	<!--begin::Chat-->
-	<div class="app-navbar-item ms-1 ms-md-4">
-		<!--begin::Menu wrapper-->
-		<div class="btn btn-icon btn-custom btn-icon-muted btn-active-light btn-active-color-primary w-35px h-35px position-relative" id="kt_drawer_chat_toggle">
-			<i class="ki-duotone ki-message-text-2 fs-2">
-				<span class="path1"></span>
-				<span class="path2"></span>
-				<span class="path3"></span>
-			</i>
-			<span class="bullet bullet-dot bg-success h-6px w-6px position-absolute translate-middle top-0 start-50 animation-blink"></span>
-		</div>
-		<!--end::Menu wrapper-->
-	</div>
-	<!--end::Chat-->
+
 
 	<!--begin::Theme mode-->
 	<div class="app-navbar-item ms-1 ms-md-4">
@@ -134,7 +97,12 @@
 	<div class="app-navbar-item ms-1 ms-md-4" id="kt_header_user_menu_toggle">
 		<!--begin::Menu wrapper-->
 		<div class="cursor-pointer symbol symbol-35px" data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-attach="parent" data-kt-menu-placement="bottom-end">
-		<img src="<?php echo URLROOT . "/public/" . $_SESSION['user_image']->profileimage; ?>" class="rounded-3" alt="user" />
+			<?php if ($_SESSION['user_role'] == 'Student' || $_SESSION['user_role'] == 'Lecturer') : ?>
+				<img src="<?php echo URLROOT . "/public/" . $_SESSION['user_image']->profileimage; ?>" class="rounded-3" alt="user" />
+			<?php endif; ?>
+			<?php if ($_SESSION['user_role'] == 'Administrator' || $_SESSION['user_role'] == 'Master Administrator') : ?>
+				<img src="<?php echo URLROOT . "/public/images/dummy/user.png"; ?>" class="rounded-3" alt="dropdown" />
+			<?php endif; ?>
 		</div>
 		<!--begin::User account menu-->
 		<div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg menu-state-color fw-semibold py-4 fs-6 w-275px" data-kt-menu="true">
@@ -142,9 +110,11 @@
 			<div class="menu-item px-3">
 				<div class="menu-content d-flex align-items-center px-3">
 					<!--begin::Avatar-->
-					<div class="symbol symbol-50px me-5">
-						<img alt="Logo" src="<?php echo URLROOT . "/public/" . $_SESSION['user_image']->profileimage; ?>" />
-					</div>
+					<?php if ($_SESSION['user_role'] == 'Student' || $_SESSION['user_role'] == 'Lecturer') : ?>
+						<div class="symbol symbol-50px me-5">
+							<img alt="Logo" src="<?php echo URLROOT . "/public/" . $_SESSION['user_image']->profileimage; ?>" />
+						</div>
+					<?php endif; ?>
 					<!--end::Avatar-->
 					<!--begin::Username-->
 					<div class="d-flex flex-column">
@@ -152,7 +122,7 @@
 							<!-- <span class="badge badge-light-success fw-bold fs-8 px-2 py-1 ms-2">Pro</span>s -->
 						</div>
 						<a href="#" class="fw-semibold text-muted text-hover-primary fs-7"><?php if (isset($_SESSION['email'])) echo $_SESSION['email'] ?></a>
-					</div> 
+					</div>
 					<!--end::Username-->
 				</div>
 			</div>
@@ -160,12 +130,12 @@
 			<!--begin::Menu separator-->
 			<div class="separator my-2"></div>
 			<!--end::Menu separator-->
-			<?php if ($_SESSION['user_role'] == 'Student' || $_SESSION['user_role'] == 'Lecturer' ) : ?>
-			<!--begin::Menu item-->
-			<div class="menu-item px-5">
-				<a href="<?php echo URLROOT; ?>/pages/edit_profile" class="menu-link px-5">Profile</a>
-			</div>
-			<!--end::Menu item-->
+			<?php if ($_SESSION['user_role'] == 'Student' || $_SESSION['user_role'] == 'Lecturer') : ?>
+				<!--begin::Menu item-->
+				<div class="menu-item px-5">
+					<a href="<?php echo URLROOT; ?>/pages/edit_profile" class="menu-link px-5">Profile</a>
+				</div>
+				<!--end::Menu item-->
 			<?php endif; ?>
 			<!--begin::Menu item-->
 			<div class="menu-item px-5">
@@ -180,24 +150,6 @@
 					<span class="menu-title">My Subscription</span>
 					<span class="menu-arrow"></span>
 				</a> -->
-				<!--begin::Menu sub-->
-				<div class="menu-sub menu-sub-dropdown w-175px py-4">
-					
-					<!--begin::Menu separator-->
-					<div class="separator my-2"></div>
-					<!--end::Menu separator-->
-					<!--begin::Menu item-->
-					<div class="menu-item px-3">
-						<div class="menu-content px-3">
-							<label class="form-check form-switch form-check-custom form-check-solid">
-								<input class="form-check-input w-30px h-20px" type="checkbox" value="1" checked="checked" name="notifications" />
-								<span class="form-check-label text-muted fs-7">Notifications</span>
-							</label>
-						</div>
-					</div>
-					<!--end::Menu item-->
-				</div>
-				<!--end::Menu sub-->
 			</div>
 			<!--end::Menu item-->
 			<!--begin::Menu item-->
@@ -287,62 +239,9 @@
 			</div>
 			<!--end::Menu item-->
 			<!--begin::Menu item-->
-			<div class="menu-item px-5" data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-placement="left-start" data-kt-menu-offset="-15px, 0">
-				<!-- <a href="#" class="menu-link px-5">
-					<span class="menu-title position-relative">Language
-						<span class="fs-8 rounded bg-light px-3 py-2 position-absolute translate-middle-y top-50 end-0">English
-							<img class="w-15px h-15px rounded-1 ms-2" src="<?php echo URLROOT ?>/public/assets/media/flags/united-states.svg" alt="" /></span></span>
-				</a> -->
-				<!--begin::Menu sub-->
-				<div class="menu-sub menu-sub-dropdown w-175px py-4">
-					<!--begin::Menu item-->
-					<div class="menu-item px-3">
-						<a href="account/settings.html" class="menu-link d-flex px-5 active">
-							<span class="symbol symbol-20px me-4">
-								<img class="rounded-1" src="<?php echo URLROOT ?>/public/assets/media/flags/united-states.svg" alt="" />
-							</span>English</a>
-					</div>
-					<!--end::Menu item-->
-					<!--begin::Menu item-->
-					<div class="menu-item px-3">
-						<a href="account/settings.html" class="menu-link d-flex px-5">
-							<span class="symbol symbol-20px me-4">
-								<img class="rounded-1" src="<?php echo URLROOT ?>/public/assets/media/flags/spain.svg" alt="" />
-							</span>Spanish</a>
-					</div>
-					<!--end::Menu item-->
-					<!--begin::Menu item-->
-					<div class="menu-item px-3">
-						<a href="account/settings.html" class="menu-link d-flex px-5">
-							<span class="symbol symbol-20px me-4">
-								<img class="rounded-1" src="<?php echo URLROOT ?>/public/assets/media/flags/germany.svg" alt="" />
-							</span>German</a>
-					</div>
-					<!--end::Menu item-->
-					<!--begin::Menu item-->
-					<div class="menu-item px-3">
-						<a href="account/settings.html" class="menu-link d-flex px-5">
-							<span class="symbol symbol-20px me-4">
-								<img class="rounded-1" src="<?php echo URLROOT ?>/public/assets/media/flags/japan.svg" alt="" />
-							</span>Japanese</a>
-					</div>
-					<!--end::Menu item-->
-					<!--begin::Menu item-->
-					<div class="menu-item px-3">
-						<a href="account/settings.html" class="menu-link d-flex px-5">
-							<span class="symbol symbol-20px me-4">
-								<img class="rounded-1" src="<?php echo URLROOT ?>/public/assets/media/flags/france.svg" alt="" />
-							</span>French</a>
-					</div>
-					<!--end::Menu item-->
-				</div>
-				<!--end::Menu sub-->
-			</div>
-			<!--end::Menu item-->
-			<!--begin::Menu item-->
-			<div class="menu-item px-5 my-1">
+			<!-- <div class="menu-item px-5 my-1">
 				<a href="account/settings.html" class="menu-link px-5">Account Settings</a>
-			</div>
+			</div> -->
 			<!--end::Menu item-->
 			<!--begin::Menu item-->
 			<div class="menu-item px-5">
