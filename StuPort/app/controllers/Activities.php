db24b5283d1c13b897fbe17e921255ab28a12881
YIKHONGLEE <128198038+YIKHONGLEE@users.noreply.github.com>
2024-01-14 17:48:20 +0800

    s

--- StuPort/app/controllers/Activities.php
+++ StuPort/app/controllers/Activities.php
@@ -36,17 +36,17 @@ class Activities extends Controller
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             // Sanitize and validate input data
             $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
-            $data = [
-                'user_id' => $_SESSION['user_id'],
-                'title' => trim($_POST['title']),
-                'activity_desc' => trim($_POST['activity_desc']),
-                'category' => trim($_POST['category']),
-                'act_datetime' => trim($_POST['act_datetime']),
-                'location' => trim($_POST['location']),
-                'organizer_name' => trim($_POST['organizer_name']),
-                'skill_acquired' => trim($_POST['skill_acquired']),
-                'attachment' => trim($_POST['attachment'])               
-            ];
+            // $data = [
+            //     'user_id' => $_SESSION['user_id'],
+            //     'title' => trim($_POST['title']),
+            //     'activity_desc' => trim($_POST['activity_desc']),
+            //     'category' => trim($_POST['category']),
+            //     'act_datetime' => trim($_POST['act_datetime']),
+            //     'location' => trim($_POST['location']),
+            //     'organizer_name' => trim($_POST['organizer_name']),
+            //     'skill_acquired' => trim($_POST['skill_acquired']),
+            //     'attachment' => $location               
+            // ];
 
              //sanitize post data
              $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
@@ -64,14 +64,14 @@ class Activities extends Controller
                  $ext = pathinfo($filename, PATHINFO_EXTENSION);
                  if (!array_key_exists($ext, $allowed)) {
                      $_SESSION['failed'] = "Error: You cannot upload files of this type!";
-                     header("Location: " . URLROOT . "/pages/edit_profile");
+                     header("Location: " . URLROOT . "/activities/create");
                  }
  
                  $email = $_SESSION['email']; //Email will be the name of folder created
                  $maxsize = 5 * 1024 * 1024;
                  if ($filesize > $maxsize) {
                      $_SESSION['failed'] = "Error: File size is larger than the allowed limit.";
-                     header("Location: " . URLROOT . "/pages/edit_profile");
+                     header("Location: " . URLROOT . "/activities/create");
                  }
                  $location = "images/activities/" . $email;
  
@@ -102,6 +102,18 @@ class Activities extends Controller
                  $_SESSION['failed'] = "Error: There was an error uploading your file!";
                  header("Location: " . URLROOT . "/activities/create");
              }
+
+             $data = [
+                'user_id' => $_SESSION['user_id'],
+                'title' => trim($_POST['title']),
+                'activity_desc' => trim($_POST['activity_desc']),
+                'category' => trim($_POST['category']),
+                'act_datetime' => trim($_POST['act_datetime']),
+                'location' => trim($_POST['location']),
+                'organizer_name' => trim($_POST['organizer_name']),
+                'skill_acquired' => trim($_POST['skill_acquired']),
+                'attachment' => $location               
+            ];
 
     
     
