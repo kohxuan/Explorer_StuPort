9766ea4fa1b95145c7b8b57ae65c9467b2f2f9cc
Koh Su Xuan <ksx0428@gmail.com>
2024-01-14 10:14:55 +0800

    Update view_profile_student.php

--- StuPort/app/views/pages/view_profile_student.php
+++ StuPort/app/views/pages/view_profile_student.php
@@ -1,3 +1,5 @@
+<?php require APPROOT . '/views/includes/head_metronic.php';?>
+<?php require APPROOT . '/views/includes/begin_app.php';?>
 <div id="kt_app_content" class="app-content flex-column-fluid">
     <!--begin::Content container-->
     <div id="kt_app_content_container" class="app-container container-fluid">
@@ -504,3 +506,6 @@
     </div> -->
     </div>
 </div>
+
+<?php require APPROOT . '/views/includes/end_app.php'; ?>
+<?php require APPROOT . '/views/includes/footer_metronic.php';?>
