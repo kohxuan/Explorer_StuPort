c4f16bcbc9c85abf09905c79b976136055c4f1c3
Koh Su Xuan <ksx0428@gmail.com>
2024-01-14 11:19:49 +0800

    logo sidebar

--- StuPort/app/views/includes/begin_app.php
+++ StuPort/app/views/includes/begin_app.php
@@ -52,8 +52,8 @@
                             <div class="app-sidebar-logo px-6" id="kt_app_sidebar_logo">
                                 <!--begin::Logo image-->
                                 <a href="index.html">
-                                    <img alt="Logo" src="<?php echo URLROOT ?>/public/assets/media/logos/default-dark.svg" class="h-25px app-sidebar-logo-default" />
-                                    <img alt="Logo" src="<?php echo URLROOT ?>/public/assets/media/logos/default-small.svg" class="h-20px app-sidebar-logo-minimize" />
+                                    <img alt="Logo" src="<?php echo URLROOT ?>/public/assets/media/logos/yvsidebarlogo.png" class="h-50px app-sidebar-logo-default" />
+                                    <img alt="Logo" src="<?php echo URLROOT ?>/public/assets/media/logos/yvsidebarlogomin.jpg" class="h-35px app-sidebar-logo-minimize" />
                                 </a>
 
                                     <div id="kt_app_sidebar_toggle" class="app-sidebar-toggle btn btn-icon btn-shadow btn-sm btn-color-muted btn-active-color-primary h-30px w-30px position-absolute top-50 start-100 translate-middle rotate" data-kt-toggle="true" data-kt-toggle-state="active" data-kt-toggle-target="body" data-kt-toggle-name="app-sidebar-minimize">
--- StuPort/app/views/pages/view_profile_student.php
+++ StuPort/app/views/pages/view_profile_student.php
@@ -1,5 +1,5 @@
-<?php require APPROOT . '/views/includes/head_metronic.php';?>
-<?php require APPROOT . '/views/includes/begin_app.php';?>
+<?php require APPROOT . '/views/includes/head_metronic.php'; ?>
+<?php require APPROOT . '/views/includes/begin_app.php'; ?>
 <div id="kt_app_content" class="app-content flex-column-fluid">
     <!--begin::Content container-->
     <div id="kt_app_content_container" class="app-container container-fluid">
@@ -508,4 +508,4 @@
 </div>
 
 <?php require APPROOT . '/views/includes/end_app.php'; ?>
-<?php require APPROOT . '/views/includes/footer_metronic.php';?>
+<?php require APPROOT . '/views/includes/footer_metronic.php'; ?>
